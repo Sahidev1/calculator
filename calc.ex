ad2785937572d8fec4b49d9f81a10203cd777109
Sahidev1 <theoddrogue@gmail.com>
2024-12-10 17:00:43 +0100

    symbolic arithmetic calculator now fully supports addition, subtraction

--- calc.ex
+++ calc.ex
@@ -1,9 +1,15 @@
 
 # Grammar:
-# E -> E + E | E - E| (E) | V | (-V)
-# V -> INTEGER
+# E -> T + E | T - E | T
+# T -> INTEGER | (E) | -T
+
+
+#steps:
+#1) Tokenize input
+#2) parse token sequence and build AST
+#3 optional) Evaluate AST
 defmodule Calc do
-  @type op():: :PLUS | :MINUS
+  @type op():: :PLUS | :MINUS |:NEGATE
   @type delim():: :LPARAN | :RPARAN
   @type literal():: integer()
   @type token():: op() | delim() | literal()
@@ -15,17 +21,91 @@ defmodule Calc do
 
   def example do
     input = " 1 + 3345 - (41 + 6)  -334   + 2141 - (123 + 5654 - 5)+1"
-    buildAST(input)
+    ast = buildAST(input)
+    v=evalAST(ast)
+    {ast, v}
+  end
 
+  def eval(expr) do
+    ast = buildAST(expr)
+    evalAST(ast)
   end
 
   def buildAST(input) do
     input = String.replace(input, ~r/\s+/, "")
     input = String.to_charlist(input)
-    token = getTokens(input, [])
-    IO.inspect(input)
-    parse(token, true)
+    tokens = getTokens(input, [])
+    {ast,_} = parseE(tokens)
+    ast
   end
+
+  def evalAST({:LITERAL, v}) do v end
+  def evalAST({:NEGATE, expr}) do
+    -evalAST(expr)
+  end
+  def evalAST({:PLUS, left, right}) do
+    evalAST(left) + evalAST(right)
+  end
+  def evalAST({:MINUS, left, right}) do
+    evalAST(left) - evalAST(right)
+  end
+
+  def parseE(tokens) do
+    {a, tokens} = parseT(tokens)
+    #IO.puts("parseE")
+    #IO.inspect(a)
+    {nextTok, tokens} = scanToks(tokens)
+    #IO.inspect(nextTok)
+
+    case nextTok do
+      :PLUS->
+        {b, tokens} = parseT(tokens)
+        parseE_prime({:PLUS, a, b}, tokens)
+      :MINUS->
+        {b, tokens} = parseT(tokens)
+        parseE_prime({:MINUS, a, b}, tokens)
+      _-> {a, tokens}
+    end
+  end
+
+  def parseE_prime(a_prime={op, a, b}, tokens) do
+    {nextTok, tokens} = scanToks(tokens)
+    #IO.puts("parseE prime a_prime")
+    #IO.inspect(nextTok)
+    case nextTok do
+      :PLUS ->
+        {b, tokens} = parseT(tokens)
+        parseE_prime({:PLUS, a_prime, b}, tokens)
+      :MINUS ->
+        {b, tokens} = parseT(tokens)
+        parseE_prime({:MINUS, a_prime, b}, tokens)
+      _->{a_prime, tokens}
+    end
+  end
+
+  def parseT([]) do {nil, []} end
+  def parseT(tokens) do
+    {a, tokens} = scanToks(tokens)
+    #IO.puts("parseT token:")
+    #IO.inspect(a)
+
+    case a do
+      {:LITERAL, v} -> {a, tokens}
+      [_|_] -> #this happens if if we get an expression between LPARAN and RPARAN, this is expression in between
+        {res, _} = parseE(a)
+        {res, tokens}
+      :MINUS->
+          #{b, tokens} = scanToks(tokens)
+          {res, tokens} = parseT(tokens)
+          {{:NEGATE, res}, tokens}
+
+      _->{{:ERROR, "invalid term "}, tokens}
+    end
+  end
+
+
+
+
 
   def isDigit?(c) do
     c >= 48 && c <= 57
@@ -36,53 +116,18 @@ defmodule Calc do
     {toconsume, rest}
   end
 
+  def consumeSubExpr(tokens=[:RPARAN|rest], subToks, stack=[]) do {subToks, rest} end
+  def consumeSubExpr(tokens=[:RPARAN|rest], subToks, stack=[popped|remstack]) do consumeSubExpr(rest, subToks++[:RPARAN], remstack) end
+  def consumeSubExpr(tokens=[:LPARAN| rest], subtoks, stack) do consumeSubExpr(rest, subtoks++[:LPARAN], [:LPARAN|stack]) end
+  def consumeSubExpr(tokens=[curr|rest], subToks, stack) do consumeSubExpr(rest,subToks++[curr], stack) end
 
-  def consumeSubExpr(tokens=[:RPARAN|rest], subToks) do
-    {subToks, rest}
+  def scanToks([]) do consume([]) end
+  def scanToks(tokens=[:LPARAN| rest]) do
+    consumeSubExpr(rest, [], [])
   end
-  def consumeSubExpr(tokens=[curr|rest], subToks) do consumeSubExpr(rest,subToks++[curr]) end
-
-  def parse([], _) do nil end
-  def parse(tokens, first=true) do
-    {tok, tokens} = consume(tokens)
-    case tok do
-      {:LITERAL, v} ->
-        if(tokens != []) do
-          {op, tokens} = consume(tokens)
-          {op, tok, parse(tokens, false)}
-        else
-          tok
-        end
-      :PLUS -> {:ERROR, "invalid token ordering"} #dont allow +k unary
-      :MINUS ->
-        if(tokens == []) do {:MINUS, 0, tok} else {:MINUS, 0, parse(tokens, true)} end
-      :LPARAN ->
-        {subToks, tokens} = consumeSubExpr(tokens, [])
-        {op, tokens} = consume(tokens)
-        {op, parse(subToks, true), parse(tokens, false)}
-      true->{:ERROR, "invalid token"}
-    end
+  def scanToks(tokens=[nextTok|rest]) do
+    consume(tokens)
   end
-
-  def parse(tokens, first=false) do
-    {tok, tokens} = consume(tokens)
-    case tok do
-      {:LITERAL, v} ->
-        if(tokens != []) do
-          {op, tokens} = consume(tokens)
-          {op, tok, parse(tokens, false)}
-        else
-          tok
-        end
-      :LPARAN ->
-        {subToks, tokens} = consumeSubExpr(tokens, [])
-        {op, tokens} = consume(tokens)
-        {op, parse(subToks, true), parse(tokens, false)}
-      true->{:ERROR, "invalid token"}
-    end
-  end
-
-
 
   def getTokens(input=[], tokens) do tokens end
   def getTokens(input, tokens) do
